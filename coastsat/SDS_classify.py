c0c6217af9104143aa28160355bb9253682f34f9
Kilian Vos <z5030440@ad.unsw.edu.au>
2019-11-20 16:03:48 +1100

    update on classifier evaluation

--- coastsat/SDS_classify.py
+++ coastsat/SDS_classify.py
@@ -125,7 +125,9 @@ def label_images(metadata,settings):
             ax.axis('off')  
             ax.imshow(im_RGB)
             implot = ax.imshow(im_viz, alpha=0.6)            
-            
+            filename = filenames[i][:filenames[i].find('.')][:-4] 
+            ax.set_title(filename)
+           
             ##############################################################
             # select image to label
             ##############################################################           
@@ -295,7 +297,6 @@ def label_images(metadata,settings):
                 im_labels[selector_land.im_bool] = settings['labels']['other land features']  
                 
                 # save labelled image
-                filename = filenames[i][:filenames[i].find('.')][:-4] 
                 ax.set_title(filename)
                 fig.canvas.draw_idle()                         
                 fp = os.path.join(filepath_train,settings['inputs']['sitename'])
@@ -354,9 +355,64 @@ def load_labels(train_sites, settings):
         print('%s : %d pixels'%(key,len(features[key])))
     
     return features
-        
 
-def check_classifier(classifier, metadata, settings):
+def format_training_data(features, classes, labels):
+    # initialize X and y
+    X = np.nan*np.ones((1,features[classes[0]].shape[1]))
+    y = np.nan*np.ones((1,1))
+    # append row of features to X and corresponding label to y 
+    for i,key in enumerate(classes):
+        y = np.append(y, labels[i]*np.ones((features[key].shape[0],1)), axis=0)
+        X = np.append(X, features[key], axis=0)
+    # remove first row
+    X = X[1:,:]; y = y[1:]
+    # replace nans with something close to 0
+    # training algotihms cannot handle nans
+    X[np.isnan(X)] = 1e-9 
+    
+    return X, y
+
+def plot_confusion_matrix(y_true,y_pred,classes,normalize=False,cmap=plt.cm.Blues):
+    """
+    Function copied from the scikit-learn examples (https://scikit-learn.org/stable/)
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    # compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+        
+    # plot confusion matrix
+    fig, ax = plt.subplots(figsize=(6,6), tight_layout=True)
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+#    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]), ylim=[3.5,-0.5],
+           xticklabels=classes, yticklabels=classes,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black",
+                    fontsize=12)
+    fig.tight_layout()
+    return ax
+
+def evaluate_classifier(classifier, metadata, settings):
     """
     Interactively visualise the new classifier.
 
@@ -378,8 +434,6 @@ def check_classifier(classifier, metadata, settings):
             True if there is an issue with the cloud mask and sand pixels are being masked on the images
         labels: dict
             the label name (key) and label number (value) for each class
-        tolerance: float
-            tolerance used for flood fill when labelling the sand pixels
         filepath_train: str
             directory in which to save the labelled data
 
@@ -387,6 +441,11 @@ def check_classifier(classifier, metadata, settings):
     -----------
 
     """  
+    # create folder
+    fp = os.path.join(os.getcwd(), 'evaluation')
+    if not os.path.exists(fp):
+        os.makedirs(fp)
+        
     # initialize figure
     fig,ax = plt.subplots(1,2,figsize=[17,10],sharex=True, sharey=True,constrained_layout=True)
     mng = plt.get_current_fig_manager()                                         
@@ -452,117 +511,168 @@ def check_classifier(classifier, metadata, settings):
             ax[0].axis('off')
             ax[1].axis('off')
             filename = filenames[i][:filenames[i].find('.')][:-4] 
-            ax[0].set_title(filename)
-
-            # set a key event to accept/reject the detections (see https://stackoverflow.com/a/15033071)
-            # this variable needs to be immuatable so we can access it after the keypress event
-            key_event = {}
-            def press(event):
-                # store what key was pressed in the dictionary
-                key_event['pressed'] = event.key
-            # let the user press a key, right arrow to keep the image, left arrow to skip it
-            # to break the loop the user can press 'escape'
-            while True:
-                btn_keep = ax[0].text(1, 0.9, 'next image ⇨', size=14, ha="right", va="top",
-                                    transform=ax[0].transAxes,
-                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
-                btn_skip = ax[0].text(0, 0.9, '⇦ next site', size=14, ha="left", va="top",
-                                    transform=ax[0].transAxes,
-                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
-                btn_esc = ax[0].text(0.5, 0, '<esc> to quit', size=14, ha="center", va="top",
-                                    transform=ax[0].transAxes,
-                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
-                plt.draw()
-                fig.canvas.mpl_connect('key_press_event', press)
-                plt.waitforbuttonpress()
-                # after button is pressed, remove the buttons
-                btn_skip.remove()
-                btn_keep.remove()
-                btn_esc.remove()
-                # keep/skip image according to the pressed key, 'escape' to break the loop
-                if key_event.get('pressed') == 'right':
-                    skip_image = False
-                    break
-                elif key_event.get('pressed') == 'left':
-                    skip_image = True
-                    break
-                elif key_event.get('pressed') == 'escape':
-                    plt.close()
-                    raise StopIteration('User cancelled visualising images')
-                else:
-                    plt.waitforbuttonpress()
-            
+            ax[0].set_title(filename)        
             # save figure
-            if settings['save_figure']:
-                fp = os.path.join(settings['filepath_train'],
-                                  settings['inputs']['sitename']+'_check')
-                if not os.path.exists(fp):
-                    os.makedirs(fp)
-                fig.savefig(os.path.join(fp,filename+'.jpg'), dpi=200)
-            if skip_image:
-                for cax in fig.axes:
-                   cax.clear()
-                break
-            else:
-                for cax in fig.axes:
-                   cax.clear()
-                continue
-            
+            fig.savefig(os.path.join(fp,settings['inputs']['sitename'] + filename[:19] +'.jpg'), dpi=200)
+            # clear axes
+            for cax in fig.axes:
+               cax.clear()
+   
     # close the figure at the end
     plt.close()
     
-def format_training_data(features, classes, labels):
-    # initialize X and y
-    X = np.nan*np.ones((1,features[classes[0]].shape[1]))
-    y = np.nan*np.ones((1,1))
-    # append row of features to X and corresponding label to y 
-    for i,key in enumerate(classes):
-        y = np.append(y, labels[i]*np.ones((features[key].shape[0],1)), axis=0)
-        X = np.append(X, features[key], axis=0)
-    # remove first row
-    X = X[1:,:]; y = y[1:]
-    # replace nans with something close to 0
-    # training algotihms cannot handle nans
-    X[np.isnan(X)] = 1e-9 
+#def check_classifier(classifier, metadata, settings):
+#    """
+#    Interactively visualise the new classifier.
+#
+#    KV WRL 2018
+#
+#    Arguments:
+#    -----------
+#        classifier: joblib object
+#            Multilayer Perceptron to be used for image classification
+#        metadata: dict
+#            contains all the information about the satellite images that were downloaded
+#        settings: dict
+#            contains the following fields:
+#        cloud_thresh: float
+#            value between 0 and 1 indicating the maximum cloud fraction in the image that is accepted
+#        sitename: string
+#            name of the site (also name of the folder where the images are stored)
+#        cloud_mask_issue: boolean
+#            True if there is an issue with the cloud mask and sand pixels are being masked on the images
+#        labels: dict
+#            the label name (key) and label number (value) for each class
+#        tolerance: float
+#            tolerance used for flood fill when labelling the sand pixels
+#        filepath_train: str
+#            directory in which to save the labelled data
+#
+#    Returns:
+#    -----------
+#
+#    """  
+#    # initialize figure
+#    fig,ax = plt.subplots(1,2,figsize=[17,10],sharex=True, sharey=True,constrained_layout=True)
+#    mng = plt.get_current_fig_manager()                                         
+#    mng.window.showMaximized()  
+#
+#    # create colormap for labels
+#    cmap = cm.get_cmap('tab20c')
+#    colorpalette = cmap(np.arange(0,13,1))
+#    colours = np.zeros((3,4))
+#    colours[0,:] = colorpalette[5]
+#    colours[1,:] = np.array([204/255,1,1,1])
+#    colours[2,:] = np.array([0,91/255,1,1])
+#    # loop through satellites
+#    for satname in metadata.keys():
+#        filepath = SDS_tools.get_filepath(settings['inputs'],satname)
+#        filenames = metadata[satname]['filenames']
+#        # loop through images
+#        for i in range(len(filenames)):   
+#            # image filename
+#            fn = SDS_tools.get_filenames(filenames[i],filepath, satname)
+#            # read and preprocess image
+#            im_ms, georef, cloud_mask, im_extra, im_QA, im_nodata = SDS_preprocess.preprocess_single(fn, satname, settings['cloud_mask_issue'])
+#            # calculate cloud cover
+#            cloud_cover = np.divide(sum(sum(cloud_mask.astype(int))),
+#                                    (cloud_mask.shape[0]*cloud_mask.shape[1]))
+#            # skip image if cloud cover is above threshold
+#            if cloud_cover > settings['cloud_thresh'] or cloud_cover == 1:
+#                continue
+#            im_RGB = SDS_preprocess.rescale_image_intensity(im_ms[:,:,[2,1,0]], cloud_mask, 99.9)
+#            # classify image
+#            features = SDS_shoreline.calculate_features(im_ms,
+#                                    cloud_mask, np.ones(cloud_mask.shape).astype(bool))
+#            features[np.isnan(features)] = 1e-9
+#            # remove NaNs and clouds
+#            vec_cloud = cloud_mask.reshape(cloud_mask.shape[0]*cloud_mask.shape[1])
+#            vec_nan = np.any(np.isnan(features), axis=1)
+#            vec_mask = np.logical_or(vec_cloud, vec_nan)
+#            features = features[~vec_mask, :]
+#            # predict with NN classifier
+#            labels = classifier.predict(features)
+#            # recompose image
+#            vec_classif = np.nan*np.ones((cloud_mask.shape[0]*cloud_mask.shape[1])) 
+#            vec_classif[~vec_mask] = labels
+#            im_classif = vec_classif.reshape((cloud_mask.shape[0], cloud_mask.shape[1]))
+#            # labels
+#            im_sand = im_classif == 1
+#            # remove small patches of sand
+#    #        im_sand = morphology.remove_small_objects(im_sand, min_size=25, connectivity=2)
+#            im_swash = im_classif == 2
+#            im_water = im_classif == 3
+##            im_land = im_classif == 0
+#            im_labels = np.stack((im_sand,im_swash,im_water), axis=-1)   
+#            # create classified image
+#            im_class = np.copy(im_RGB)
+#            for k in range(0,im_labels.shape[2]):
+#                im_class[im_labels[:,:,k],0] = colours[k,0]
+#                im_class[im_labels[:,:,k],1] = colours[k,1]
+#                im_class[im_labels[:,:,k],2] = colours[k,2]        
+#            # show images
+#            ax[0].imshow(im_RGB)
+#            ax[1].imshow(im_RGB)
+#            ax[1].imshow(im_class, alpha=0.5)
+#            ax[0].axis('off')
+#            ax[1].axis('off')
+#            filename = filenames[i][:filenames[i].find('.')][:-4] 
+#            ax[0].set_title(filename)
+#
+#            # set a key event to accept/reject the detections (see https://stackoverflow.com/a/15033071)
+#            # this variable needs to be immuatable so we can access it after the keypress event
+#            key_event = {}
+#            def press(event):
+#                # store what key was pressed in the dictionary
+#                key_event['pressed'] = event.key
+#            # let the user press a key, right arrow to keep the image, left arrow to skip it
+#            # to break the loop the user can press 'escape'
+#            while True:
+#                btn_keep = ax[0].text(1, 0.9, 'next image ⇨', size=14, ha="right", va="top",
+#                                    transform=ax[0].transAxes,
+#                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
+#                btn_skip = ax[0].text(0, 0.9, '⇦ next site', size=14, ha="left", va="top",
+#                                    transform=ax[0].transAxes,
+#                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
+#                btn_esc = ax[0].text(0.5, 0, '<esc> to quit', size=14, ha="center", va="top",
+#                                    transform=ax[0].transAxes,
+#                                    bbox=dict(boxstyle="square", ec='k',fc='w'))
+#                plt.draw()
+#                fig.canvas.mpl_connect('key_press_event', press)
+#                plt.waitforbuttonpress()
+#                # after button is pressed, remove the buttons
+#                btn_skip.remove()
+#                btn_keep.remove()
+#                btn_esc.remove()
+#                # keep/skip image according to the pressed key, 'escape' to break the loop
+#                if key_event.get('pressed') == 'right':
+#                    skip_image = False
+#                    break
+#                elif key_event.get('pressed') == 'left':
+#                    skip_image = True
+#                    break
+#                elif key_event.get('pressed') == 'escape':
+#                    plt.close()
+#                    raise StopIteration('User cancelled visualising images')
+#                else:
+#                    plt.waitforbuttonpress()
+#            
+#            # save figure
+#            if settings['save_figure']:
+#                fp = os.path.join(settings['filepath_train'],
+#                                  settings['inputs']['sitename']+'_check')
+#                if not os.path.exists(fp):
+#                    os.makedirs(fp)
+#                fig.savefig(os.path.join(fp,filename+'.jpg'), dpi=200)
+#            if skip_image:
+#                for cax in fig.axes:
+#                   cax.clear()
+#                break
+#            else:
+#                for cax in fig.axes:
+#                   cax.clear()
+#                continue
+#            
+#    # close the figure at the end
+#    plt.close()
     
-    return X, y
-
-def plot_confusion_matrix(y_true,y_pred,classes,normalize=False,cmap=plt.cm.Blues):
-    """
-    Function copied from the scikit-learn examples (https://scikit-learn.org/stable/)
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    # compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-        
-    # plot confusion matrix
-    fig, ax = plt.subplots(figsize=(6,6), tight_layout=True)
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-#    ax.figure.colorbar(im, ax=ax)
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]), ylim=[3.5,-0.5],
-           xticklabels=classes, yticklabels=classes,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black",
-                    fontsize=12)
-    fig.tight_layout()
-    return ax

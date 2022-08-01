13f89818742e7aefccf96c12c94ce5d2eec8216c
kvos <voskilian@gmail.com>
2022-08-01 23:57:28 +1000

    update notebook, script and readme

--- example.py
+++ example.py
@@ -16,7 +16,6 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 plt.ion()
 import pandas as pd
-from datetime import datetime
 from coastsat import SDS_download, SDS_preprocess, SDS_shoreline, SDS_tools, SDS_transects
 
 # region of interest (longitude, latitude in WGS84)
@@ -32,10 +31,10 @@ polygon = [[[151.301454, -33.700754],
 polygon = SDS_tools.smallest_rectangle(polygon)
 
 # date range
-dates = ['2017-12-01', '2018-01-01']
+dates = ['1984-01-01', '2022-01-01']
 
 # satellite missions
-sat_list = ['S2']
+sat_list = ['L5','L7','L8']
 collection = 'C01' # choose Landsat collection 'C01' or 'C02'
 # name of the site
 sitename = 'NARRA'
@@ -72,24 +71,25 @@ metadata = SDS_download.get_metadata(inputs)
 # settings for the shoreline extraction
 settings = { 
     # general parameters:
-    'cloud_thresh': 0.5,        # threshold on maximum cloud cover
-    'output_epsg': 3857,        # epsg code of spatial reference system desired for the output
+    'cloud_thresh': 0.1,        # threshold on maximum cloud cover
+    'dist_clouds': 300,         # ditance around clouds where shoreline can't be mapped
+    'output_epsg': 28356,       # epsg code of spatial reference system desired for the output
     # quality control:
-    'check_detection': True,    # if True, shows each shoreline detection to the user for validation
+    'check_detection': False,    # if True, shows each shoreline detection to the user for validation
     'adjust_detection': False,  # if True, allows user to adjust the postion of each shoreline by changing the threhold
     'save_figure': True,        # if True, saves a figure showing the mapped shoreline for each image
     # [ONLY FOR ADVANCED USERS] shoreline detection parameters:
-    'min_beach_area': 4500,     # minimum area (in metres^2) for an object to be labelled as a beach
-    'min_length_sl': 200,       # minimum length (in metres) of shoreline perimeter to be valid
+    'min_beach_area': 1000,     # minimum area (in metres^2) for an object to be labelled as a beach
+    'min_length_sl': 500,       # minimum length (in metres) of shoreline perimeter to be valid
     'cloud_mask_issue': False,  # switch this parameter to True if sand pixels are masked (in black) on many images  
     'sand_color': 'default',    # 'default', 'latest', 'dark' (for grey/black sand beaches) or 'bright' (for white sand beaches)
-    'pan_off': True,            # True to switch pansharpening off
+    'pan_off': False,           # True to switch pansharpening off for Landsat 7/8/9 imagery
     # add the inputs defined previously
     'inputs': inputs,
 }
 
 # [OPTIONAL] preprocess images (cloud masking, pansharpening/down-sampling)
-SDS_preprocess.save_jpg(metadata, settings)
+# SDS_preprocess.save_jpg(metadata, settings)
 
 # [OPTIONAL] create a reference shoreline (helps to identify outliers and false detections)
 settings['reference_shoreline'] = SDS_preprocess.get_reference_sl(metadata, settings)
@@ -123,7 +123,8 @@ for i in range(len(output['shorelines'])):
     sl = output['shorelines'][i]
     date = output['dates'][i]
     plt.plot(sl[:,0], sl[:,1], '.', label=date.strftime('%d-%m-%Y'))
-plt.legend()    
+# plt.legend()    
+fig.savefig(os.path.join(inputs['filepath'], inputs['sitename'], 'mapped_shorelines.jpg'),dpi=200)
 
 #%% 4. Shoreline analysis
 
@@ -131,6 +132,10 @@ plt.legend()
 filepath = os.path.join(inputs['filepath'], sitename)
 with open(os.path.join(filepath, sitename + '_output' + '.pkl'), 'rb') as f:
     output = pickle.load(f) 
+# remove duplicates (images taken on the same date by the same satellite)
+output = SDS_tools.remove_duplicates(output)
+# remove inaccurate georeferencing (set threshold to 10 m)
+output = SDS_tools.remove_inaccurate_georef(output, 10)
 
 # now we have to define cross-shore transects over which to quantify the shoreline changes
 # each transect is defined by two points, its origin and a second point that defines its orientation
@@ -170,13 +175,28 @@ for i,key in enumerate(list(transects.keys())):
     plt.plot(transects[key][:,0],transects[key][:,1],'k-',lw=1)
     plt.text(transects[key][0,0]-100, transects[key][0,1]+100, key,
                 va='center', ha='right', bbox=dict(boxstyle="square", ec='k',fc='w'))
+fig.savefig(os.path.join(inputs['filepath'], inputs['sitename'], 'mapped_shorelines.jpg'),dpi=200)
 
-# intersect the transects with the 2D shorelines to obtain time-series of cross-shore distance
-# (also saved a .csv file with the time-series, dates are in UTC time)
+#%% Option 1: Compute intersections with quality-control parameters (recommended)
+
+settings_transects = { # parameters for computing intersections
+                      'along_dist':          25,        # along-shore distance to use for computing the intersection
+                      'min_points':          3,         # minimum number of shoreline points to calculate an intersection
+                      'max_std':             15,        # max std for points around transect
+                      'max_range':           30,        # max range for points around transect
+                      'min_chainage':        -100,      # largest negative value along transect (landwards of transect origin)
+                      'multiple_inter':      'auto',    # mode for removing outliers ('auto', 'nan', 'max')
+                      'prc_multiple':         0.1,      # percentage to use in 'auto' mode to switch from 'nan' to 'max'
+                     }
+cross_distance = SDS_transects.compute_intersection_QC(output, transects, settings_transects) 
+
+#%% Option 2: Conpute intersection in a simple way (no quality-control)
+
 settings['along_dist'] = 25
 cross_distance = SDS_transects.compute_intersection(output, transects, settings) 
 
-# plot the time-series
+#%% Plot the time-series of cross-shore shoreline change
+
 fig = plt.figure(figsize=[15,8], tight_layout=True)
 gs = gridspec.GridSpec(len(cross_distance),1)
 gs.update(left=0.05, right=0.95, bottom=0.05, top=0.95, hspace=0.05)
@@ -186,11 +206,23 @@ for i,key in enumerate(cross_distance.keys()):
     ax = fig.add_subplot(gs[i,0])
     ax.grid(linestyle=':', color='0.5')
     ax.set_ylim([-50,50])
-    ax.plot(output['dates'], cross_distance[key]- np.nanmedian(cross_distance[key]), '-o', ms=6, mfc='w')
+    ax.plot(output['dates'], cross_distance[key]- np.nanmedian(cross_distance[key]), '-o', ms=4, mfc='w')
     ax.set_ylabel('distance [m]', fontsize=12)
     ax.text(0.5,0.95, key, bbox=dict(boxstyle="square", ec='k',fc='w'), ha='center',
             va='top', transform=ax.transAxes, fontsize=14) 
-    
+fig.savefig(os.path.join(inputs['filepath'], inputs['sitename'], 'time_series_raw.jpg'),dpi=200)
+  
+# save time-series in a .csv file
+out_dict = dict([])
+out_dict['dates'] = output['dates']
+for key in transects.keys():
+    out_dict['Transect '+ key] = cross_distance[key]
+df = pd.DataFrame(out_dict)
+fn = os.path.join(settings['inputs']['filepath'],settings['inputs']['sitename'],
+                  'transect_time_series.csv')
+df.to_csv(fn, sep=',')
+print('Time-series of the shoreline change along the transects saved as:\n%s'%fn)
+
 #%% 4. Tidal correction
     
 # For this example, measured water levels for Sydney are stored in a csv file located in /examples.
@@ -250,3 +282,107 @@ for i,key in enumerate(cross_distance.keys()):
     ax.text(0.5,0.95, key, bbox=dict(boxstyle="square", ec='k',fc='w'), ha='center',
             va='top', transform=ax.transAxes, fontsize=14)
 ax.legend()
+
+#%% 5. Time-series post-processing
+
+# load mapped shorelines from 1984
+filename_output = os.path.join(os.getcwd(),'examples','NARRA_output.pkl')
+with open(filename_output, 'rb') as f:
+    output = pickle.load(f) 
+
+# plot the mapped shorelines
+fig = plt.figure(figsize=[15,8], tight_layout=True)
+plt.axis('equal')
+plt.xlabel('Eastings')
+plt.ylabel('Northings')
+plt.grid(linestyle=':', color='0.5')
+plt.title('%d shorelines mapped at Narrabeen from 1984'%len(output['shorelines']))
+for i in range(len(output['shorelines'])):
+    sl = output['shorelines'][i]
+    date = output['dates'][i]
+    plt.plot(sl[:,0], sl[:,1], '.', label=date.strftime('%d-%m-%Y'))
+for i,key in enumerate(list(transects.keys())):
+    plt.plot(transects[key][0,0],transects[key][0,1], 'bo', ms=5)
+    plt.plot(transects[key][:,0],transects[key][:,1],'k-',lw=1)
+    plt.text(transects[key][0,0]-100, transects[key][0,1]+100, key,
+                va='center', ha='right', bbox=dict(boxstyle="square", ec='k',fc='w'))
+
+# load long time-series (1984-2021)
+filepath = os.path.join(os.getcwd(),'examples','NARRA_time_series_tidally_corrected.csv')
+df = pd.read_csv(filepath, parse_dates=['dates'])
+dates = [_.to_pydatetime() for _ in df['dates']]
+cross_distance = dict([])
+for key in transects.keys():
+    cross_distance[key] = np.array(df[key])
+  
+#%% 5.1 Remove outliers
+
+# plot Otsu thresholds for the mapped shorelines
+fig,ax = plt.subplots(1,1,figsize=[12,5],tight_layout=True)
+ax.grid(which='major',ls=':',lw=0.5,c='0.5')
+ax.plot(output['dates'],output['MNDWI_threshold'],'o-',mfc='w')
+ax.axhline(y=-0.5,ls='--',c='r',label='otsu_threshold limits')
+ax.axhline(y=0,ls='--',c='r')
+ax.set(title='Otsu thresholds on MNDWI for the %d shorelines mapped'%len(output['shorelines']),
+       ylim=[-0.6,0.2],ylabel='otsu threshold')
+ax.legend(loc='upper left')
+fig.savefig(os.path.join(inputs['filepath'], inputs['sitename'], 'otsu_threhsolds.jpg'))
+
+# remove outliers in the time-series (despiking)
+settings_outliers = {'otsu_threshold':     [-.5,0],        # min and max intensity threshold use for contouring the shoreline
+                     'max_cross_change':   40,             # maximum cross-shore change observable between consecutive timesteps
+                     'plot_fig':           True,           # whether to plot the intermediate steps
+                    }
+cross_distance = SDS_transects.reject_outliers(cross_distance,output,settings_outliers)
+
+#%% 5.2 Seasonal averaging
+
+# compute seasonal averages along each transect
+season_colors = {'DJF':'C3', 'MAM':'C1', 'JJA':'C2', 'SON':'C0'}
+for key in cross_distance.keys():
+    chainage = cross_distance[key]
+    # remove nans
+    idx_nan = np.isnan(chainage)
+    dates_nonan = [dates[_] for _ in np.where(~idx_nan)[0]]
+    chainage = chainage[~idx_nan] 
+    
+    # compute shoreline seasonal averages (DJF, MAM, JJA, SON)
+    dict_seas, dates_seas, chainage_seas, list_seas = SDS_transects.seasonal_average(dates_nonan, chainage)
+    
+    # plot seasonal averages
+    fig,ax=plt.subplots(1,1,figsize=[14,4],tight_layout=True)
+    ax.grid(b=True,which='major', linestyle=':', color='0.5')
+    ax.set_title('Time-series at %s'%key, x=0, ha='left')
+    ax.set(ylabel='distance [m]')
+    ax.plot(dates_nonan, chainage,'+', lw=1, color='k', mfc='w', ms=4, alpha=0.5,label='raw datapoints')
+    ax.plot(dates_seas, chainage_seas, '-', lw=1, color='k', mfc='w', ms=4, label='seasonally-averaged')
+    for k,seas in enumerate(dict_seas.keys()):
+        ax.plot(dict_seas[seas]['dates'], dict_seas[seas]['chainages'],
+                 'o', mec='k', color=season_colors[seas], label=seas,ms=5)
+    ax.legend(loc='lower left',ncol=6,markerscale=1.5,frameon=True,edgecolor='k',columnspacing=1)
+            
+#%% 5.3 Monthly averaging
+
+# compute monthly averages along each transect
+month_colors = plt.cm.get_cmap('tab20')
+for key in cross_distance.keys():
+    chainage = cross_distance[key]
+    # remove nans
+    idx_nan = np.isnan(chainage)
+    dates_nonan = [dates[_] for _ in np.where(~idx_nan)[0]]
+    chainage = chainage[~idx_nan] 
+    
+    # compute shoreline seasonal averages (DJF, MAM, JJA, SON)
+    dict_month, dates_month, chainage_month, list_month = SDS_transects.monthly_average(dates_nonan, chainage)
+    
+    # plot seasonal averages
+    fig,ax=plt.subplots(1,1,figsize=[14,4],tight_layout=True)
+    ax.grid(b=True,which='major', linestyle=':', color='0.5')
+    ax.set_title('Time-series at %s'%key, x=0, ha='left')
+    ax.set(ylabel='distance [m]')
+    ax.plot(dates_nonan, chainage,'+', lw=1, color='k', mfc='w', ms=4, alpha=0.5,label='raw datapoints')
+    ax.plot(dates_month, chainage_month, '-', lw=1, color='k', mfc='w', ms=4, label='monthly-averaged')
+    for k,month in enumerate(dict_month.keys()):
+        ax.plot(dict_month[month]['dates'], dict_month[month]['chainages'],
+                 'o', mec='k', color=month_colors(k), label=month,ms=5)
+    ax.legend(loc='lower left',ncol=7,markerscale=1.5,frameon=True,edgecolor='k',columnspacing=1)

0be53566e10dd56914b3bb0902ac393c97899583
Kilian Vos <voskilian@gmail.com>
2023-11-14 10:03:02 +1100

    Update SDS_download.py
    only download missing imagery

--- coastsat/SDS_download.py
+++ coastsat/SDS_download.py
@@ -119,21 +119,8 @@ def retrieve_images(inputs):
     # main loop to download the images for each satellite mission
     print('\nDownloading images:')
     suffix = '.tif'
-    
-    # check if images already exist in the data folders
-    metadata_existing = get_metadata(inputs)
-    
     for satname in im_dict_T1.keys():
-          
-        # remove from download list the images that are already existing
-        if satname in metadata_existing:
-            if len(metadata_existing[satname]['dates']) > 0:
-                last_date = metadata_existing[satname]['dates'][-1]
-                date_list = [datetime.fromtimestamp(_['properties']['system:time_start']/1000, tz=pytz.utc) for _ in im_dict_T1[satname]]
-                idx_new = np.where([_ > last_date for _ in date_list])[0]
-                # only keep images corresponding to dates that are not already existing
-                im_dict_T1[satname] = [im_dict_T1[satname][_] for _ in idx_new]
-        
+
         # print how many images will be downloaded for the users
         print('%s: %d images'%(satname,len(im_dict_T1[satname])))
 
@@ -598,6 +585,20 @@ def check_images_available(inputs):
         
     print('  Total to download: %d images'%sum_img)
 
+    # check if images already exist  
+    print('\nLooking for existing imagery...')
+    metadata_existing = get_metadata(inputs)
+    for satname in inputs['sat_list']:
+        # remove from download list the images that are already existing
+        if satname in metadata_existing:
+            if len(metadata_existing[satname]['dates']) > 0:
+                last_date = metadata_existing[satname]['dates'][-1]
+                date_list = [datetime.fromtimestamp(_['properties']['system:time_start']/1000, tz=pytz.utc) for _ in im_dict_T1[satname]]
+                idx_new = np.where([_ > last_date for _ in date_list])[0]
+                # only keep images corresponding to dates that are not already existing
+                im_dict_T1[satname] = [im_dict_T1[satname][_] for _ in idx_new]
+                print('%s: %d images already exist, %s to download'%(satname, len(date_list)-len(idx_new), len(idx_new)))
+
     # if only S2 is in sat_list, stop here as no Tier 2 for Sentinel
     if len(inputs['sat_list']) == 1 and inputs['sat_list'][0] == 'S2':
         return im_dict_T1, []
@@ -630,7 +631,7 @@ def check_images_available(inputs):
             im_dict_T2[satname] += im_list         
 
     print('  Total Tier 2: %d images'%sum_img)
-
+    
     return im_dict_T1, im_dict_T2
 
 def get_image_info(collection,satname,polygon,dates,**kwargs):

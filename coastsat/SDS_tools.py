0e662e7cac47fe21644b4a2837bda97e27a37d62
Kilian Vos <voskilian@gmail.com>
2023-11-19 21:53:06 +1100

    bug fixes

--- coastsat/SDS_tools.py
+++ coastsat/SDS_tools.py
@@ -873,8 +873,11 @@ def smallest_rectangle(polygon):
 ###################################################################################################
 
 def make_animation_mp4(filepath_images, fps, fn_out):
+    "function to create an animation with the saved figures"
     with imageio.get_writer(fn_out, mode='I', fps=fps) as writer:
         filenames = os.listdir(filepath_images)
+        # order chronologically
+        filenames = np.sort(filenames)
         for i in range(len(filenames)):
             image = imageio.imread(os.path.join(filepath_images,filenames[i]))
             writer.append_data(image)
